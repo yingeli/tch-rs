4c8d707b386b4253822763b0d35628f06ce36279
yingeli <yingeli@hotmail.com>
2025-01-30 17:23:22 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -8,6 +8,7 @@ fn main() {
             println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
             println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
             println!("cargo:rustc-link-arg=-ltorch");
+            println!("cargo:rustc-link-arg=-ltorch_cuda");
         }
         _ => {}
     }
